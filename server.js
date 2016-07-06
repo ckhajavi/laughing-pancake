a366086789790dd7bd481c45a284afcc564ee62b
John Duong <jhnduong@gmail.com>
2016-07-06 12:34:32 -0700

    -removed 'end' (so far I've found this is redundant to 'close'?) -changed to human readable date time -request is kept open and closed when user navigates away(is this desired behavior? should they send a response instead when it closes? although if the connection is closed does that work?)

--- server.js
+++ server.js
@@ -3,17 +3,41 @@ var emptygif = require('emptygif');
 var app = express();
 var middleWare = require('./middleWare.js');
 var callbacks = function(){
-	console.log("gothere");
+    console.log("gothere");
 }
 
 
 app.use(emptygif.emptyGif([{path:'/erreport', maxAge: 0,  callback:function(req) { console.log("gothere")} }]));
 
 app.get('/about',middleWare.Auth,function(request,response){
-	    
+        
 });
 
 
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
 
 //app.use(express.static(__dirname+"/public"));
 
@@ -21,18 +45,12 @@ app.get('/tracking_pixel.gif', function(req, res, next) {
 
     process.nextTick(function() {
         // do tracking stuff
-        console.log(Date.now())
+        console.log("connection opened at: ", getDateTime())
     });
 
-    console.log(Date.now())
-
-    req.on("end", function() {
-  // request ended normally
-  		console.log("it ended")
-	});
 
     req.on('close', function(){
-        console.log('Client closed the connection');
+        console.log('Client closed the connection at: ', getDateTime());
         emptygif.sendEmptyGif(req, res, {
             'Content-Type' : 'image/gif',
             'Content-Length' : emptygif.emptyGifBufferLength,
@@ -43,5 +61,5 @@ app.get('/tracking_pixel.gif', function(req, res, next) {
 });
 
 app.listen(3100,function(){
-	console.log('Server running on port number 3100');
+    console.log('Server running on port number 3100');
 });
